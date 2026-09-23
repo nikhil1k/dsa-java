ed8d016773547b6a39d83e0f957c117769dbdb47
Nikhil Kumar <nikrpq@gmail.com>
2026-09-23 12:25:01 +0530

    Create ArrayBasics.java
    Add basic array implementation

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,15 @@
+public class ArrayBasics {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("Array length: " + numbers.length);
+    }
+}
